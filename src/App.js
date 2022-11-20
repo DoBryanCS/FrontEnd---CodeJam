60fd49735c71c62e91503504c56c1b486da47f25
MoKhedim <mohammedkhedim123@gmail.com>
2022-11-20 09:45:19 -0500

    sessionstorage

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ import {auth} from "./firebase-config"
             event.preventDefault();
             const user = await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
             console.log(user)
-            sessionStorage.setItem("key", "value");
+            sessionStorage.setItem("email", loginEmail);
             navigate("/homePage")
         } catch (error) {
             console.log(error.message)
